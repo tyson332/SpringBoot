c29dc893b808fcd096d747bfca56139dff8c59e1
Tyson <gill.tyson332@gmail.com>
2021-04-13 17:00:00 +0530

    Fix

---
+++ src/main/java/SpringSecurity/Controller.java
@@ -0,0 +1,18 @@
+package SpringSecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping("/user")
+	public String getUser() {
+		return "Hello User";
+	}
+	
+	@GetMapping("/admin")
+	public String getAdmin() {
+		return "Hello Admin";
+	}
+}
