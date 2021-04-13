566bc496bfff611852a1042800508cf401ce17e7
Tyson <gill.tyson332@gmail.com>
2021-04-13 20:35:05 +0530

    fix

--- src/main/java/com/springSecurity/App.java
+++ src/main/java/com/springSecurity/App.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+//(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class App {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(App.class, args);
