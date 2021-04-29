19f930928d374092008fb6bedba1c7d7c85dde5f
Tyson <gill.tyson332@gmail.com>
2021-04-29 21:09:06 +0530

    Logging Tested

--- src/main/java/SpringSecurity/Controller.java
+++ src/main/java/SpringSecurity/Controller.java
@@ -1,13 +1,24 @@
 package SpringSecurity;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class Controller {
 	
+	Logger logger = LoggerFactory.getLogger(Controller.class);
+	
 	@GetMapping("/user")
 	public String getUser() {
+		logger.trace("Trace Printed");
+		logger.debug("Debug Printed");
+		logger.info("Info Printed");
+		logger.warn("Warn Printed");
+		logger.error("Error Printed");
 		return "Hello User";
 	}
 	
@@ -16,3 +27,4 @@ public class Controller {
 		return "Hello Admin";
 	}
 }
+
